b9a610e8c1adbb7ab3eaccffb5d89986bbf25a7d
teerth sharma <69573227+spiceTheCode@users.noreply.github.com>
2026-09-21 07:55:40 +0530

    peek in peek out added

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 
 var media
 var expanded = document.querySelector("#expanded")
+var autoFrame = document.querySelector(".img-container")
 
 document.querySelector("#app").innerHTML = "<p>loading...</p>";
 
@@ -10,6 +11,18 @@ document.querySelector("#waySetter").addEventListener("click", e=>{
     expanded.style.visibility="visible";
   }else
     expanded.style.visibility="hidden";
+})
+
+document.querySelector("#peekOut").addEventListener("click", e=>{
+  autoFrame.style.visibility="hidden";
+  document.querySelector("#peekOut").style.visibility="hidden"
+  document.querySelector("#peekIn").style.visibility="visible"
+})
+
+document.querySelector("#peekIn").addEventListener("click", e=>{
+  autoFrame.style.visibility="visible";
+  document.querySelector("#peekOut").style.visibility="visible"
+  document.querySelector("#peekIn").style.visibility="hidden"
 })
 
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
